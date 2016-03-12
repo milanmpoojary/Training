3e8902f80b25e2fbdb9483c3df04ffed858e8c4f
milanmpoojary <milan.kodez@gmail.com>
2016-03-12 07:10:12 -0800

    Changes

--- MVCDemo_RazorView/MVCDemo_RazorView/App_Start/RouteConfig.cs
+++ MVCDemo_RazorView/MVCDemo_RazorView/App_Start/RouteConfig.cs
@@ -11,13 +11,22 @@ namespace MVCDemo_RazorView
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            // http://localhost/scriptresource.axd/foo
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            name: "Default",                                                                    // Route Name
+            url: "{controller}/{action}/{id}",                                                  // URL With Parameters
+            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional } //Parameter defaults
+             );
+            // http://localhost/Process/
+            routes.MapRoute(
+                name: "Process",                                                                    // Route Name
+                url: "{controller}/{action}/{id}",                                                  // URL With Parameters
+                defaults: new { controller = "Process", action = "List", id = "" } //Parameter defaults
+                );
+
+
         }
     }
 }
